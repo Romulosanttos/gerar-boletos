49ecddb9a50fcb18a438346ddd25e1322be44e01
Rômulo Cabral Santos <romulosanttos1@gmail.com>
2019-05-13 09:28:47 -0300

    ajustando parametros para função createBoleto

--- examples/gerar-boleto-bradesco.js
+++ examples/gerar-boleto-bradesco.js
@@ -4,26 +4,39 @@ import createBoleto from './criarBoleto';
 
 
 const init = () => {
-	const banco = new Gerador.boleto.bancos.Bradesco();
-	const pagador = {
-		RegistroNacional: '12345678'
-	};
-	const boleto_info = {
-		valor: 100.00,
-		datas: {
-			vencimento: '02-04-2020',
-			processamento: '02-04-2019',
-			documentos: '02-04-2019'
+	const boleto = {
+		banco: new Gerador.boleto.bancos.BancoBrasil(),
+		pagador: { RegistroNacional: '12345678' },
+		beneficiario: {
+			dadosBancarios:{
+				carteira: '09',
+				agencia: '18455',
+				agenciaDigito: '4',
+				conta: '1277165',
+				contaDigito: '1' ,
+				nossoNumero: '00000000061',
+				nossoNumeroDigito: '8'
+			}
+		},
+		boleto: {
+			numeroDocumento: '1001',
+			especieDocumento: 'DM',
+			valor: 110.00,
+			datas: {
+				vencimento: '02-04-2020',
+				processamento:  '02-04-2019',
+				documentos: '02-04-2019'
+			}
 		}
 	};
-	const boleto = createBoleto(banco, pagador, boleto_info);
+	const novoBoleto = createBoleto(boleto);
 
 	const dir = './tmp/boletos';
 	if (!fs.existsSync(dir)) fs.mkdirSync(dir);
 	const writeStream = fs.createWriteStream(`${dir}/boleto-bradesco.pdf`);
 
 
-	new Gerador.boleto.Gerador(boleto).gerarPDF({
+	new Gerador.boleto.Gerador(novoBoleto).gerarPDF({
 		creditos: '',
 		stream: writeStream
 	}, (err) => {
