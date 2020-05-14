dd99e91052aa40fa701624c58ec44acbd09d6d34
romulosanttos <romulosanttos1@gmail.com>
2020-05-14 05:16:58 -0300

    WIP #3 criando classe BoletoStringify para encapsular funcoes legadas e tornar todos os dados do boleto dinâmico

--- examples/gerar-boleto-BancoDoBrasil.js
+++ examples/gerar-boleto-BancoDoBrasil.js
@@ -1,42 +1,61 @@
 const Gerador = require('../index');
-const Boletos  = require('../lib/index');
+const Boletos = require('../lib/index');
 const streamToPromise = require('../lib/utils/util');
 
 const boleto = {
-	banco: new Gerador.boleto.bancos.BancoBrasil(),
-	pagador: { RegistroNacional: '12345678' },
-	instrucoes: ['Após o vencimento Mora dia R$ 1,59','Após o vencimento, multa de 2%'],
-	beneficiario: {
-		dadosBancarios:{
-			carteira: '09',
-			agencia: '18455',
-			agenciaDigito: '4',
-			conta: '1277165',
-			contaDigito: '1' ,
-			nossoNumero: '00000000061',
-			nossoNumeroDigito: '8'
-		}
-	},
-	boleto: {
-		numeroDocumento: '1001',
-		especieDocumento: 'DM',
-		valor: 110.00,
-		datas: {
-			vencimento: '02-04-2020',
-			processamento:  '02-04-2019',
-			documentos: '02-04-2019'
-		}
-	}
+  banco: new Gerador.boleto.bancos.BancoBrasil(),
+  pagador: {
+    nome: 'José Bonifácio de Andrada',
+    registroNacional: '12345678',
+    endereco: {
+      logradouro: 'Rua Pedro Lessa, 15',
+      bairro: 'Centro',
+      cidade: 'Rio de Janeiro',
+      estadoUF: 'RJ',
+      cep: '20030-030'
+    }
+  },
+  instrucoes: ['Após o vencimento Mora dia R$ 1,59', 'Após o vencimento, multa de 2%'],
+  beneficiario: {
+    nome: 'Empresa Fictícia LTDA',
+    cnpj:'43576788000191',
+    dadosBancarios: {
+      carteira: '09',
+      agencia: '18455',
+      agenciaDigito: '4',
+      conta: '1277165',
+      contaDigito: '1',
+      nossoNumero: '00000000061',
+      nossoNumeroDigito: '8'
+    },
+    endereco: {
+      logradouro: 'Rua Pedro Lessa, 15',
+      bairro: 'Centro',
+      cidade: 'Rio de Janeiro',
+      estadoUF: 'RJ',
+      cep: '20030-030'
+    }
+  },
+  boleto: {
+    numeroDocumento: '1001',
+    especieDocumento: 'DM',
+    valor: 110.00,
+    datas: {
+      vencimento: '02-04-2020',
+      processamento: '02-04-2019',
+      documentos: '02-04-2019'
+    }
+  }
 };
 
 const novoBoleto = new Boletos(boleto);
 novoBoleto.gerarBoleto();
 
-novoBoleto.pdfFile().then(async({stream})=>{
-	// ctx.res.set('Content-type', 'application/pdf');	
-	await streamToPromise(stream);
-}).catch((error)=>{
-	return error;
+novoBoleto.pdfFile().then(async ({ stream }) => {
+  // ctx.res.set('Content-type', 'application/pdf');	
+  await streamToPromise(stream);
+}).catch((error) => {
+  return error;
 });
 
 
